d786c2c2e04b22fa84227fcec8dd4299c82f6f04
AjloriaCodex <ajloria123@gmail.com>
2024-09-16 23:25:14 +0800

    add Cvsu mental well being

--- app/navbar.js
+++ app/navbar.js
@@ -14,7 +14,7 @@ export default function BasicExample() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                     <Nav.Link href="https://visionboard-a.netlify.app/">VisionBoard</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link href="https://cvsu-mh.onrender.com/">CVSU Well Being Check</Nav.Link>
                     <NavDropdown title="Socials" id="basic-nav-dropdown">
                         <NavDropdown.Item href="    https://cara.app/ajloria/portfolio">Cara</NavDropdown.Item>
                         <NavDropdown.Item href="https://www.instagram.com/alvin.etc/">
